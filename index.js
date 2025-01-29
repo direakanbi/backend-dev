15d7f62b2c61013b580772261a6fc4830121e304
direakanbi <direakanbi@icloud.com>
2025-01-29 02:02:52 -0800

    Created simple API for returning json

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000
+
+const email = "direakanbi@icloud.com"
+const github_url = "github.com/direakanbi"
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/api/mybackend-details', (req, res) => {
+    res.json({
+        email: email,
+        current_datetime: new Date().toISOString(),
+        github_url: github_url
+    })
+}) 
+
+app.listen(port, () => {
+    console.log("Server is listening: ", port)
+})
